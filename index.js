b821392d3d73c3663cf8fd8b50a14e3a5d8ea211
traoredramane <trdramane98@gmail.com>
2023-07-18 21:09:10 +0400

    premier commit

---
+++ index.js
@@ -0,0 +1,19 @@
+alert("Le fichier fonctionne !");
+//ceci est un commentaire
+console.log("Hello");
+
+let maVariableSuperCool = "KamelCase";
+
+console.log(maVariableSuperCool);
+
+var unTexte = "Voici un texte"
+
+console.log(unTexte);
+
+unTexte = "Nouveau texte..."; 
+
+console.log(unTexte);
+
+let unChiffre = 24;
+unChiffre = 12;  
+console.log(unChiffre);
