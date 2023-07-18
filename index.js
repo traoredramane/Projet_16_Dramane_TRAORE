9e00bef0992fc4787f9b482e1386754ef93380d4
traoredramane <trdramane98@gmail.com>
2023-07-18 22:51:38 +0400

    deuxieme commit

--- index.js
+++ index.js
@@ -1,19 +1,108 @@
-alert("Le fichier fonctionne !");
-//ceci est un commentaire
-console.log("Hello");
+// alert("Le fichier fonctionne !");
+// //ceci est un commentaire
+// console.log("Hello");
 
-let maVariableSuperCool = "KamelCase";
+// let maVariableSuperCool = "KamelCase";
 
-console.log(maVariableSuperCool);
+// console.log(maVariableSuperCool);
 
-var unTexte = "Voici un texte"
+// var unTexte = "Voici un texte"
 
-console.log(unTexte);
+// console.log(unTexte);
 
-unTexte = "Nouveau texte..."; 
+// unTexte = "Nouveau texte..."; 
 
-console.log(unTexte);
+// console.log(unTexte);
 
-let unChiffre = 24;
-unChiffre = 12;  
-console.log(unChiffre);
+// let unChiffre = 24;
+// unChiffre = 12;  
+// console.log(unChiffre);
+
+// let chaine = "Je suis l'autre chaine de caractère"
+
+// let nouvelleChaine = "Chaine précédente :" + chaine
+
+// console.log(nouvelleChaine);
+
+// let string = "je suis une chaine"
+// let number = 24;
+// let boolean = true;
+// let array = ["je", "suis", 24, false];
+
+// let objet ={
+//     prenom: "audrey",
+//     age: 34,
+//     ville: "Bordeaux",
+// };
+
+// // console.log(typeof array);
+// // console.log(typeof number);
+
+// let arbre =null;
+// console.log(arbre);
+// // console.log(typeof arbre);
+
+// console.log(4 * 5);
+// console.log(4 ** 5);
+
+// let total = 0;
+
+// total++;
+
+// console.log(total);
+
+// let total = 2;
+
+// total *=5;
+
+// console.log(total);
+
+// let total = 2;
+// let x = 4;
+// x++;
+
+// total=x
+// console.log(total);
+// // total =++x;
+
+// console.log(total);
+// let x = 2;
+// let x = 6;
+// let x = 5;
+// let y = 5;
+//  if (x < y) {
+//     console.log("x est inferieur à y");
+//  } else {
+//      console.log("x est supérieur à y");
+//  }
+//  if (!x) {
+//      console.log("x existe");
+//  }
+ 
+//  if ((x === y)) {
+//      console.log("True !");
+//  } else {
+//      console.log("false !");
+//  }
+//  let x = 5;
+//  let y =5;
+//  if ((x !== y)) {
+//     console.log("True !");
+// } else {
+//     console.log("false !");
+// } 
+
+let x = 5;
+let y = "5";
+
+// if (x === y ||  x > 3) {
+//     console.log("True !");
+// }else {
+//     console.log("false !");
+// }
+
+if (x == y && x >3 && y <10) {
+    console.log("True !");
+}else {
+    console.log("false !");
+}
